8a99b90a388135b80a43f6da220a64f0602c8883
Harmeet Singh Chugga <harmeetsinghchugga@gmail.com>
2025-10-15 08:51:30 +0530

    basepath error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import { useState } from 'react'
-import {  Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from '../components/navbar';
 import Home from './pages/Home';
 import About from './pages/About';
-import Navbar from '../components/navbar';
 import Login from './components/Login.jsx';
 import Profile from './components/Profile.jsx';
-      
 
-  
-function App() {      
-  
-
-  return (      
-    <>
+function App() {
+  return (
+    <Router basename="/your-repo-name"> {/* 👈 replace with your actual repo name */}
       <div>
-      <Navbar />   
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/profile' element={<Profile />} />
-      </Routes>
-      </div>    
-    </>
-  )
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
