102196e847a2227a124821b8e7e7f02ab7472224
Harmeet Singh Chugga <harmeetsinghchugga@gmail.com>
2025-10-24 18:04:05 +0530

    updated home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,14 @@ const Home = () => {
         <div>
             <h1>Welcome to the Home Page</h1>
             <p>This is the home page of our application.</p>
-            <p> this app is using apis from https://dummyjson.com/ for authentication.</p>
+            <p>The app is live at https://harmeet773.github.io/dummy-authentication-using-redux</p>
+
+            <p> This app is using api's from https://dummyjson.com/ for authentication.
+                Login component fetches access token and refresh token from 'https://dummyjson.com/auth/login' 
+                by provideing user credentials and stores in it redux store.
+                </p>
+                <p> Profile component fetches user data from 'https://dummyjson.com/auth/me' by providing access token</p>
+
         </div>
     );
 }
